e9f76998e3bdd93e2f17a4ca460ea50309b7c32b
amadub <aaronmawilliams@gmail.com>
2021-04-17 09:26:21 -0400

    updated backend url

--- src/apis/randomPuns.js
+++ src/apis/randomPuns.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: 'https://randompuns-api.herokuapp.com'
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://randompuns-api.herokuapp.com'
+    // baseURL: 'http://localhost:3001'
 });
